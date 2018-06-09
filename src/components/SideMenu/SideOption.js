09f190001675654cc856d867acd75f17b657da03
Nate Alba <nate@thechgrp.com>
2018-06-08 17:35:44 -0700

    just checking

--- src/components/SideMenu/SideOption.js
+++ src/components/SideMenu/SideOption.js
@@ -8,8 +8,6 @@ import { Link } from 'react-router-dom'
 
 import { toggleSideMenu } from '../../actions/sideMenuActions'
 
-console.log('Link', Link)
-
 class SideOption extends Component {
 	render() {
 		return (
